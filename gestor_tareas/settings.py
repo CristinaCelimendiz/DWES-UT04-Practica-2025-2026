bed19b0b7f19909a4641ecad10cf8396721d3510
Cristina Celimendiz <ccelimendizfx@fpvirtualaragon.es>
2026-01-11 05:49:08 +0100

    PostgreSQL operativo, migraciones hechas y admin funcional

--- gestor_tareas/settings.py
+++ gestor_tareas/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'gestor_tareas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestor_tareas_db',
+        'USER': 'gestor_user',
+        'PASSWORD': 'pirineus',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
